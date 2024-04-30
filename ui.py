39975e7e7a725f687d813078e8ad8d11ff591e96
Veregorn <rjbarco@gmail.com>
2024-04-30 07:08:43 +0200

    Graphic UI created

--- ui.py
+++ ui.py
@@ -1,2 +1,40 @@
+from tkinter import *
+
+# Constants
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self) -> None:
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.geometry("350x550+500+200") # width x height + x_offset + y_offset
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="Some Question Text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        pass
+
+    def true_pressed(self):
+        pass
+
+    def false_pressed(self):
+        pass
