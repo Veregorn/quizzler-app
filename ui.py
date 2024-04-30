f8baa10c9ddd580c02a57d7e7deec09be8d8b8e5
Veregorn <rjbarco@gmail.com>
2024-04-30 10:55:25 +0200

    When you reach the last question you are informed and buttons are disabled

--- ui.py
+++ ui.py
@@ -34,9 +34,15 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
         self.canvas.config(bg="white")
+        # Test if there are still questions
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_pressed(self):
         self.update_score(self.quiz.check_answer("True"))
